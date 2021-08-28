62916dd1cadd5343624c1223279d9edf5e192ae1
Derek <derek.huynen@gmail.com>
2021-08-27 23:49:16 -0700

    updated

--- src/Pages/Menu.js
+++ src/Pages/Menu.js
@@ -1,89 +1,47 @@
 import React, {useEffect, useState} from "react";
 import '../CSS/menu.css'
-import bblSort from '../Algos/Bubble'
-import Selection from '../Algos/Selection'
+import {menu} from '../JSON/menu'
 
-const randomArray = (size) => {
-    return shuffleArray(Array.from(Array(size + 1).keys()).slice(1))
-}
 
-const shuffleArray = array => {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        const temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-    return array;
+
+
+const sections = (sectionItems,height) => {
+    return (
+            <div className={"section-container"} >
+                <h2>{sectionItems.name}</h2>
+                <div className={"food-container"} style={{height: height}}>
+                    {sectionItems.items.map((sectionItem)=> {
+                        return (
+
+                            <div className={"food-item"}>
+                                <h3>{sectionItem.item}</h3>
+                                <h4>{sectionItem.price}</h4>
+                                <p>{sectionItem.description}</p>
+                            </div>
+                        )
+
+                    })}
+                </div>
+
+
+            </div>
+        )
+
 }
 
 
 export default function Menu() {
 
-    const arrLength = 25;
-    const [arr, setArr] = useState({arr: randomArray(arrLength), front: 0, middle: 0, end: 0});
 
-
-
-    const displayBox = (number) => {
-        const newHeight = number * 20;
-
-        if (number === arr.end) {
-            return (
-                <div className={"bar"}
-                     style={{height: newHeight, marginTop: 500 - newHeight, backgroundColor: 'orange'}}>
-                    <h4>{number}</h4>
-                </div>
-            )
-        } else if (number === arr.middle) {
-            return (
-                <div className={"bar"}
-                     style={{height: newHeight, marginTop: 500 - newHeight, backgroundColor: 'yellow'}}>
-                    <h4>{number}</h4>
-                </div>
-            )
-        } else if (number === arr.front) {
-            return (
-                <div className={"bar"}
-                     style={{height: newHeight, marginTop: 500 - newHeight, backgroundColor: 'green'}}>
-                    <h4>{number}</h4>
-                </div>
-            )
-        } else {
-            return (
-                <div className={"bar"} style={{height: newHeight, marginTop: 500 - newHeight}}>
-                    <h4>{number}</h4>
-                </div>
-            )
-        }
-    }
 
     return (
-        <div className={"menuContainer"}>
-            <div className={"sortingBox"}>
-                {arr.arr.map((num) => {
-                    return displayBox(num)
-                })}
-            </div>
-            <button onClick={() => {
-                setArr(prevState => ({
-                    ...prevState,
-                    arr: randomArray(arrLength),
-                    front: 0,
-                    middle: 0,
-                    end: 0
-                }));
-                bblSort(arr.arr, setArr)
-            }}>Bubble
-            </button>
-            <button onClick={() => {
+        <div className={"menu-container"}>
 
-                const newArray = randomArray(25)
-                console.log(newArray)
-                setArr({arr: [...newArray], front: 0,middle:0,  end: 0})
-                Selection(arr.arr, setArr);
-            }}>Selection
-            </button>
+            {console.log(menu)}
+            {sections(menu["Appetizers"], 650)}
+            {sections(menu["SpecialtiesPizzas"], 400)}
+            {sections(menu["FavesPizzas"], 400)}
+
         </div>
 
     )
--- src/Pages/SortingAlgo.js
+++ src/Pages/SortingAlgo.js
@@ -1,21 +1,90 @@
 import React, { useState} from "react";
-import '../CSS/menu.css'
+import '../CSS/algo.css'
+import bblSort from '../Algos/Bubble'
+import Selection from '../Algos/Selection'
+
+const randomArray = (size) => {
+    return shuffleArray(Array.from(Array(size + 1).keys()).slice(1))
+}
+
+const shuffleArray = array => {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
+
+
+export default function SortingAlgo() {
+
+    const arrLength = 25;
+    const [arr, setArr] = useState({arr: randomArray(arrLength), front: 0, middle: 0, end: 0});
 
 
 
+    const displayBox = (number) => {
+        const newHeight = number * 20;
 
-
-export default function Menu() {
-
-
+        if (number === arr.end) {
+            return (
+                <div className={"bar"}
+                     style={{height: newHeight, marginTop: 500 - newHeight, backgroundColor: 'orange'}}>
+                    <h4>{number}</h4>
+                </div>
+            )
+        } else if (number === arr.middle) {
+            return (
+                <div className={"bar"}
+                     style={{height: newHeight, marginTop: 500 - newHeight, backgroundColor: 'yellow'}}>
+                    <h4>{number}</h4>
+                </div>
+            )
+        } else if (number === arr.front) {
+            return (
+                <div className={"bar"}
+                     style={{height: newHeight, marginTop: 500 - newHeight, backgroundColor: 'green'}}>
+                    <h4>{number}</h4>
+                </div>
+            )
+        } else {
+            return (
+                <div className={"bar"} style={{height: newHeight, marginTop: 500 - newHeight}}>
+                    <h4>{number}</h4>
+                </div>
+            )
+        }
+    }
 
     return (
+        <div className={"menuContainer"}>
+            <div className={"sortingBox"}>
+                {arr.arr.map((num) => {
+                    return displayBox(num)
+                })}
+            </div>
+            <button onClick={() => {
+                setArr(prevState => ({
+                    ...prevState,
+                    arr: randomArray(arrLength),
+                    front: 0,
+                    middle: 0,
+                    end: 0
+                }));
+                bblSort(arr.arr, setArr)
+            }}>Bubble
+            </button>
+            <button onClick={() => {
 
-        <div>
-            MENU
+                const newArray = randomArray(25)
+                console.log(newArray)
+                setArr({arr: [...newArray], front: 0,middle:0,  end: 0})
+                Selection(arr.arr, setArr);
+            }}>Selection
+            </button>
         </div>
-
-
 
     )
 }
